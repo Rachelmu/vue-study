307a5869b0c991524c8ab92af5da69b40e6c19d8
mumu <mumu@mumudeMacBook-Pro.local>
2020-08-18 16:32:53 +0800

    frist commit

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    // 其余代码可以保持不变，仍然可以实现同样的功能。如果你想给拆分出的文件命名，可以尝试一下 webpack 提供的 Magic Comments（魔法注释）：
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import { createStore } from 'vuex'
+import moduleA from './modules/moduleA'
+export default createStore({
+  state: {
+    groups: [1]
+  },
+  mutations,
+  actions,
+  modules: {
+    moduleA
+  },
+  getters:{
+    getGroups(state){
+      return state.groups
+    }
+  }
+})
