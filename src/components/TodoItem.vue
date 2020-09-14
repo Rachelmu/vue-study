5901b1fbab4bf64f71c7ec6026035cc684d0676c
Rachelmu <631032057@qq.com>
2020-09-14 17:43:22 +0800

    todoList

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,18 @@
+// 子组件
+<template>
+  <div>
+    <li class="item">
+      <input v-model="checked" type="checkbox" />
+      <slot name="item" :checked="checked"></slot> // 将checked的值传递给父组件
+    </li>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      checked: false
+    };
+  }
+};
+</script>
---
+++ src/views/TodoList.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="toList">
+        <input type="text" v-model="info" />
+        <button @click="addItem">添加</button>
+        <ul>
+            <TodoTtem v-if="(item, index) in listData" :key="index">
+              <template v-slot:item="itemProps"> // 这是个具名插槽
+                // 其中itemProps的值就是子组件传递过来的对象
+                <span
+                    :style="{
+                    fontSize: '20px',
+                    color: itemProps.checked ? 'yellow' : 'blue'
+                    }"
+                    >{{ item }}</span
+                >
+                </template>
+            </TodoItem>
+        </ul>
+    </div>
+</template>
+<script>
+import TodoItem from "../components/TodoItem";
+export default {
+  components: {
+    TodoItem
+  },
+  data() {
+    return {
+      info: "",
+      listData: []
+    };
+  },
+  methods: {
+    addItem() {
+      this.listData.push(this.info);
+      this.info = "";
+    }
+  }
+};
+</script>
+<style lang="less" scoped>
+
+</style>
