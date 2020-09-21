71bd5e00a2347bdf703a3c17612e23fdbbdd9740
Rachelmu <631032057@qq.com>
2020-09-21 17:19:42 +0800

    vue custom

---
+++ src/components/second-hand-house.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="second-hand-house">
+    <div class="master-bedroom">
+      <!--插槽可以指定默认值，如果外部调用组件时没有修改插槽内容，则使用默认插槽-->
+      <slot>
+        <span>这里有一张水床，玩的够嗨</span>
+        <span>还有一个衣柜，有点旧了</span>
+      </slot>
+    </div>
+    <!--这是次卧-->
+    <div class="secondary-bedroom">
+      <!--次卧使用具名插槽-->
+      <slot name="secondard">
+        <span>这里有一张婴儿床</span>
+      </slot>
+    </div>
+  </div>
+</template>
+
---
+++ src/views/Custom.vue
@@ -0,0 +1,28 @@
+<template>
+  <custom-form size="small">
+    <custom-form-item label="姓名">
+      <custom-input v-model="formData.name" />
+    </custom-form-item>
+  </custom-form>
+</template>
+<script>
+import CustomForm from './custom/custom-form'
+import CustomFormItem from './custom/custom-form-item'
+import CustomInput from './custom/custom-input'
+export default {
+  components: {
+    CustomForm,
+    CustomFormItem,
+    CustomInput
+  },
+  data() {
+    return {
+      formData: {
+        name: '',
+        age: 0
+      }
+    }
+  }
+}
+</script>
+
